02fb09c908d319d5728bcd58b9da834ec9c25b88
Elliot Morrison <elliotmorrison58@gmail.com>
2022-09-01 09:30:52 -0500

    design update and added more drawer options

--- src/components/NavigatorComponent.vue
+++ src/components/NavigatorComponent.vue
@@ -1,7 +1,7 @@
 <template>
 <section>
     <!-- Navbar -->
-    <div class="navbar bg-slate-600 text-white ">
+    <div class="navbar bg-slate-800 text-white ">
         <div class="flex-none">
             <label for="my-drawer" class="btn btn-square btn-ghost">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -11,19 +11,20 @@
             {{props.pageName}}
         </div>
         <div class="flex-none">
-            <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-square btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+            <div class="dropdown h-12 dropdown-end">
+            <label tabindex="0" class="btn btn-circle btn-ghost avatar">
+                <div class="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" />
+                </div>
             </label>
-            <ul tabindex="1" class="bg-neutral mt-3 p-2 shadow menu menu-compact dropdown-content  rounded-box w-52">
+            <ul tabindex="1" class="bg-slate-800 mt-3 p-2 shadow menu menu-compact dropdown-content  rounded w-64">
                 <li>
                 <a class="justify-between">
                     Profile
-                    <span class="badge">New</span>
                 </a>
                 </li>
                 <li><a>Settings</a></li>
-                <li><a>Logout</a></li>
+                <li @click="Logout"><a>Logout</a></li>
             </ul>
             </div>
         </div>
@@ -38,22 +39,56 @@
         </div> 
         <div class="drawer-side">
             <label for="my-drawer" class="drawer-overlay "></label>
-            <ul class="menu p-4 overflow-y-auto w-80 sticky bg-base-100 text-base-content">
+            <ul class="menu p-4 overflow-y-auto w-80 h-full sticky bg-base-100 text-base-content">
             <!-- Sidebar content here -->
             <li><router-link to="/">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
             Home</router-link></li>
+            <li><router-link to="/">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"></path></svg>
+            Movies</router-link></li>
+            <li><router-link to="/">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 6.11861L9.4131 4.70551L11.5371 6.82954L14.3667 4L15.7782 5.41151L13.3137 7.87598H18C19.1046 7.87598 20 8.77141 20 9.87598V16.876C20 17.9805 19.1046 18.876 18 18.876H6C4.89543 18.876 4 17.9805 4 16.876V9.87598C4 8.77141 4.89543 7.87598 6 7.87598H9.75736L8 6.11861ZM18 9.87598H6V16.876H18V9.87598Z" fill="currentColor"/> <path d="M8 19.876H16V20.876H8V19.876Z" fill="currentColor" /></svg>
+            Series</router-link></li>
+            <li><router-link to="/">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+            Books</router-link></li>
+            <li><router-link to="/">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
+            Your Reviews</router-link></li>
+            <li><router-link to="/">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg>
+            Manage Content</router-link></li>
             <li><router-link to="/admin">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"></path></svg>
-            Admin</router-link></li>
+            Administration</router-link></li>
             
+            <li><router-link to="/" class="fixed w-72 bottom-4">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+            Settings</router-link></li>
             </ul>
         </div>
     </div>
 </section>
-    
 </template>
 <script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router'
+import { currentUserStore } from '../stores/User';
+const currentUser = currentUserStore()
+const router = useRouter()
+const route = useRoute()
+console.log(currentUser.fullName);
+
+function Logout(){
+    currentUser.$reset()
+    if(route.path == '/'){
+        console.log('on home');
+        router.go(0)
+    }else{
+        console.log('not on home');
+        router.push('/')
+    }
+}
 const props = defineProps<{
     pageName : string
 }>()
@@ -70,13 +105,13 @@ const props = defineProps<{
   width: 3px;
 }
 .content-container::-webkit-scrollbar-thumb {
-  background-color: rgba(0, 0, 0, 0.623);
+  background-color: rgba(255, 255, 255, 0.623);
   border-radius: 10px;
 }
 .hide-scrollbar::-webkit-scrollbar-thumb{
   visibility : hidden;
 }
 .content-container::-webkit-scrollbar-thumb:hover{
-  background-color: black;
+  background-color: rgb(0, 63, 99);
 }
 </style>
