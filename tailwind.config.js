3af8b84d5b8d1a10f8298da1fb4774a7ca189a01
Elliot Morrison <elliotmorrison58@gmail.com>
2022-09-01 09:26:30 -0500

    added custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,29 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ["night"],
+    themes: ["night",
+    {
+      mytheme: {
+          
+        "primary": "#fb7185",
+                 
+        "secondary": "#4b5563",
+                 
+        "accent": "#38bdf8",
+                 
+        "neutral": "#1B161D",
+                 
+        "base-100": "#FCFCFD",
+                 
+        "info": "#96D0DF",
+                 
+        "success": "#10b981",
+                 
+        "warning": "#F4AE0B",
+                 
+        "error": "#F43A25",
+      },
+    }
+  ],
   },
 }
