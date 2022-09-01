b3b2dea7ea0a057d893d9a6a589763cbec063040
Elliot Morrison <elliotmorrison58@gmail.com>
2022-09-01 09:29:25 -0500

    added pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
-
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './index.css'
 // import './samples/node-api'
-
-const app =  createApp(App).use(router).mount('#app').$nextTick(()=>{postMessage({ payload: 'removeLoading' }, '*')})
+const pinia = createPinia()
+createApp(App)
+.use(router)
+.use(pinia)
+.mount('#app')
+.$nextTick(()=>{
+    postMessage({ payload: 'removeLoading' }, '*')
+})
 
