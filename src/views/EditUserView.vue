3512ac3ab39bea14c07c5794757e8917a7c57bd8
Elliot Morrison <elliotmorrison58@gmail.com>
2022-09-01 09:27:52 -0500

    added view

---
+++ src/views/EditUserView.vue
@@ -0,0 +1,94 @@
+<template>
+<NavigatorComponent pageName="Administration">
+    <span class="flex justify-center pt-10" v-if="dataLoaded">
+        <div class="relative p-3 rounded max-w-4xl w-full bg-gray-800">
+            <h3 class="text-2xl font-bold text-center mb-5">Edit User Details</h3>
+            <div class="grid grid-cols-3 grid-flow-row gap-3">
+                <div class="grid grid-cols-3 md:grid-cols-7 gap-x-3 col-span-3">
+                    <div class="w-full col-span-1 md:col-span-3">
+                    <label class="label">
+                        <span class="label-text font-thin">First Name</span>
+                    </label>
+                    <input type="text" v-model="user.firstName" class="input font-medium input-bordered w-full"/>
+                    </div>
+                    <div class="w-full col-span-1 md:col-span-3">
+                        <label class="label">
+                            <span class="label-text font-thin">Last Name</span>
+                        </label>
+                        <input type="text" :value="user.lastName" class="input font-medium input-bordered w-full"/>
+                    </div>
+                    <div class="w-full col-span-1">
+                        <label class="label">
+                            <span class="label-text font-thin">Gender</span>
+                        </label>
+                        <select v-model="user.gender" name="gender" id="gender" class="select w-full">
+                            <option :value="gen" v-for="gen in Gender" :selected="user.gender == gen">{{gen}}</option>
+                        </select>
+                        <!-- <input type="text" :value="user.gender" class="input font-medium input-bordered w-full"/> -->
+                    </div>
+                </div>
+                <span class="col-span-3 grid grid-cols-4 gap-3">
+                    <div class="w-full col-span-4">
+                        <label class="label">
+                            <span class="label-text font-thin">Email</span>
+                        </label>
+                        <input type="text" v-model="user.email" class="input font-medium input-bordered w-full"/>
+                    </div>
+                    <div class="w-full col-span-2">
+                        <label class="label">
+                            <span class="label-text font-thin">Username</span>
+                        </label>
+                        <input type="text" v-model="user.userName" class="input font-medium input-bordered w-full"/>
+                    </div>
+                    <div class="w-full col-span-2">
+                        <label class="label">
+                            <span class="label-text font-thin">Phone Number</span>
+                        </label>
+                        <input type="text" v-model="user.phoneNumber" class="input font-medium input-bordered w-full"/>
+                    </div>
+                </span>
+            </div>
+            <div class="modal-action">
+                <label for="viewUser" class="btn btn-info btn-sm">save</label>
+            </div>
+        </div>
+    </span>
+</NavigatorComponent>
+</template>
+<script  lang="ts">
+import { useRoute } from "vue-router";
+import NavigatorComponent from "../components/NavigatorComponent.vue";
+import { defineComponent } from "vue";
+import { Role, User, Gender } from "@prisma/client";
+import DataService from "../DataService";
+
+export default defineComponent({
+    name:'',
+    components:{NavigatorComponent},
+    data(){
+        return{
+            user : <User>({}),
+            dataLoaded : false,
+            Role,
+            Gender,
+        }
+    },
+    async created(){
+        const route = useRoute()
+        const Id = route.params.id 
+        if(Id){
+            this.user = await DataService.getUser(Id as string)
+            this.dataLoaded = true
+            console.log(this.user);
+        }else{
+            console.log(this.user);
+        }
+    }
+})
+
+
+
+
+
+
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,69 @@
+<template>
+<span class="flex justify-center items-center min-h-screen bg-gray-400">
+    <div class="w-full max-w-lg h-fit max-h-2xl bg-base-300 shadow-2xl shadow-black bg-opacity-75 rounded-md pt-10 pb-5 px-12">
+        <p class="text-3xl font-bold text-center">Sign In</p>
+        <div class="w-1/1 my-3">
+            <div class="form-control w-full">
+            <label class="label">
+                <span class="label-text font-bold">Username</span>
+            </label>
+            <input type="text" placeholder="Enter Username" v-model="user.userName" class="input input-bordered w-full" />
+            </div>
+            <div class="form-control w-full">
+            <label class="label">
+                <span class="label-text font-bold">Password</span>
+            </label>
+            <input type="password" placeholder="**********" v-model="user.passwordHash" class="input input-bordered w-full" />
+            </div>
+        </div>
+        <p class="text-error text-sm italic">{{loginError}}</p>
+        <span class="flex justify-between items-center mt-8">
+            <span>
+                <a href="#" class="link hover:text-indigo-600 no-underline">Register</a>
+                 | 
+                <a href="#" class="link hover:text-indigo-600 no-underline">Reset Password</a>
+            </span>
+            <button class="btn btn-info" @click="login">
+            <svg v-if="loading" class="w-6 h-6 animate-spin" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
+            <p v-else>Login</p>
+            </button>
+        </span>
+    </div>
+</span>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { User } from ".prisma/client";
+import DataService from "../DataService";
+import { currentUserStore } from '../stores/User'
+import { LoginResponse } from '../Interfaces'
+export default defineComponent({
+    name:'LoginView',
+    data(){
+        return {
+            user : <User>({
+                passwordHash : '',
+                userName : '',
+            }),
+            loginError : <string>(''),
+            loading : false
+        }
+    },
+    methods:{
+        async login(){
+            this.loginError = ''
+            this.loading = true
+            let currentUser = currentUserStore()
+            let result: LoginResponse = await DataService.login(this.user)
+            console.log(result)
+            if(result.login){
+                currentUser.setCurrentUser(result.user as User)
+                this.$router.push(this.$route.query.redirect as string || '/')
+            }else{
+                this.loading = false
+                this.loginError = result.message
+            }
+        }
+    }
+})
+</script>
