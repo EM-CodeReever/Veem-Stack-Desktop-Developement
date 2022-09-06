034c17a706ad57ca3301de06aa102f92e65ffe56
Elliot Morrison <elliotmorrison58@gmail.com>
2022-09-06 15:10:55 -0500

    added user validation class

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -5,3 +5,14 @@ export interface LoginResponse {
     message : string,
     user : User | null
 }
+
+export class UserValidation {
+    static  email(emailString:string):boolean{
+        const regex = new RegExp(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)/);
+        return regex.test(emailString)
+    }
+    static phone(number:string):boolean {
+        const regex = new RegExp(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im);
+        return regex.test(number);
+    }
+}
